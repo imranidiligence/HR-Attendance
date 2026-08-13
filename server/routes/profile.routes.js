31027fe6d653311b2d96a56d59be74c6940c0e54
khan-faizan-ali <k.faizan@i-diligence.com>
2026-08-13 16:13:58 +0530

    add address routes

--- server/routes/profile.routes.js
+++ server/routes/profile.routes.js
@@ -5,7 +5,7 @@ const fs = require("fs");
 const path = require("path");
 const auth = require("../middlewares/authMiddleware");
 const { db } = require("../db/connectDB");
-const { getOrganizationInfo, addOrganizationInfo, updateOrganizationInfo, addPersonInfo, addEducationInfo, getEducationInfo, getPersonalInfo, updatePersonalInfo, updateEducationInfo, deleteEducationInfo, addExperienceInfo, updateExperienceInfo, deleteExperienceInfo, getExperienceInfo, getContactInfo, updateContactInfo, addBankInfo, getBankInfo, updateBankInfo, addBankDocInfo, getAllBankDoc, addProfileImage, getProfileImage,deleteContactInfo,addContactInfo, getNomineeInfo, addNomineeInfo, updateNomineeInfo, deleteDocument, deleteNomineeInfo } = require("../controllers/profile.controller");
+const { getOrganizationInfo, addOrganizationInfo, updateOrganizationInfo, addPersonInfo, addEducationInfo, getEducationInfo, getPersonalInfo, updatePersonalInfo, updateEducationInfo, deleteEducationInfo, addExperienceInfo, updateExperienceInfo, deleteExperienceInfo, getExperienceInfo, getContactInfo, updateContactInfo, addBankInfo, getBankInfo, updateBankInfo, addBankDocInfo, getAllBankDoc, addProfileImage, getProfileImage,deleteContactInfo,addContactInfo, getNomineeInfo, addNomineeInfo, updateNomineeInfo, deleteDocument, deleteNomineeInfo, addAddressInfo, getAddressInfo, updateAddressInfo } = require("../controllers/profile.controller");
 const { isAdmin } = require("../middlewares/roleMiddleware");
 const uploadBankDoc = require("../middlewares/uploadBankDoc");
 const uploadProfileImage = require("../middlewares/uploadProfileImage");
@@ -120,6 +120,9 @@ router.get("/bank/:emp_id", auth, getBankInfo);
 router.post("/bank/:emp_id", auth, selfOrAdminMiddleware, addBankInfo);
 router.put("/bank/:emp_id", auth, selfOrAdminMiddleware, updateBankInfo);
 
+router.post("/profile/address", auth, selfOrAdminMiddleware, addAddressInfo);
+router.get("/profile/address/:emp_id", auth, getAddressInfo);
+router.put("/profile/address/:emp_id", auth, selfOrAdminMiddleware, updateAddressInfo);
 
 
 // Document Upload Api 
